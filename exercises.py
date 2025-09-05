19bde140178621e0b362061ff4e59b1b21fbdb3f
Sharon Lee <ssmlee10@gmail.com>
2025-09-05 07:55:08 -0700

    complete exercise 3

--- exercises.py
+++ exercises.py
@@ -48,3 +48,14 @@ def combine_foods():
   return meal.rstrip(', ')
      
 print('Exercise 2:', combine_foods())
+
+# Exercise 3: Slicing Tuples
+#
+# Using the slice operator, assign a new tuple containing only the last two food strings in the foods to a variable named last_two_foods.
+
+def slice_foods():
+  foods = ('kimbap', 'ddeokbokki', 'bibimbap', 'japchae')
+  last_two_foods = foods[-2:]
+  return last_two_foods
+
+print('Exercise 3:', slice_foods())
